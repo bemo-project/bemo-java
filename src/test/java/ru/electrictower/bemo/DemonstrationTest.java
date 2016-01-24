858b0e280d0a818fbd4dc50ba5dbddf1ea768415
v1-wizard <aliaksei.boole@gmail.com>
2016-01-24 22:44:49 +0300

    Main maven structure was generated.

---
+++ src/test/java/ru/electrictower/bemo/DemonstrationTest.java
@@ -0,0 +1,17 @@
+package ru.electrictower.bemo;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by v1_wizard.
+ */
+public class DemonstrationTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+
+}
